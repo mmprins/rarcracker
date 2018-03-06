b89bd69d33d53d4e0e5ff38997d192846596c3a7
mmprins <janesyya@gmail.com>
2018-03-06 22:41:35 +0800

    class type

--- crack_dic.py
+++ crack_dic.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 # _*_ coding:utf-8 _*_
-import subprocess,os
+import subprocess,os,sys
 from multiprocessing import Pool,Manager
+class cracker(object):
+    def __init__(self,filename=None,filetype=None,dictfile=None,COUNT=0):
+        self.filename=filename
+        self.filetype=filetype
+        self.dictfile=dictfile
+        self.COUNT=COUNT
+        self.PASSWD=''
+        self.queue=Manager().Queue()
+    def cracktest(self,passwd,queue):
+        if subprocess.call(['unrar','t','-inul','-p%s'%passwd,'/tmp/ASTM_2015_Standards.rar','ASTM_2015_Standards/ASTM 2015 PART IA/ASTM 2015 Volume 01.01 Steel - Piping, Tubing, Fittings/A1014A1014M-10_Standard_Specification_for_Precipitation-Hardening_Bolting_(UNS_N07718)_for_High_Temperature_Service.pdf'],stdout = open('/dev/null','w'),stderr = subprocess.STDOUT):
+            print('%d passwd %s test faild'%(self.COUNT,passwd))
+        else:
+            print('the correct passwd is:%s'%passwd)
+            queue.put(passwd)
 COUNT=0
 PASSWD=''
 q=Manager().Queue()
