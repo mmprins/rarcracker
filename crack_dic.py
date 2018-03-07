ecfdce854ce5909c8550bd8bdcf673514cb380a2
mmprins <janesyya@gmail.com>
2018-03-07 16:22:59 +0800

    start with shell

--- crack_dic.py
+++ crack_dic.py
@@ -3,53 +3,57 @@
 import subprocess,os,sys
 from multiprocessing import Pool,Manager
 class cracker(object):
-    def __init__(self,filename=None,filetype=None,dictfile=None,COUNT=0):
+    def __init__(self,filename='test.7z',filetype=None,dictfile='test.txt',COUNT=0):
         self.filename=filename
         self.filetype=filetype
         self.dictfile=dictfile
         self.COUNT=COUNT
         self.PASSWD=''
-        self.queue=Manager().Queue()
-    def cracktest(self,passwd,queue):
-        if subprocess.call(['unrar','t','-inul','-p%s'%passwd,'/tmp/ASTM_2015_Standards.rar','ASTM_2015_Standards/ASTM 2015 PART IA/ASTM 2015 Volume 01.01 Steel - Piping, Tubing, Fittings/A1014A1014M-10_Standard_Specification_for_Precipitation-Hardening_Bolting_(UNS_N07718)_for_High_Temperature_Service.pdf'],stdout = open('/dev/null','w'),stderr = subprocess.STDOUT):
+    def _subproc(self,passwd,filename,q):
+        if subprocess.call(['7za','t','-p%s'%passwd,filename],stdout = open('/dev/null','w'),stderr = subprocess.STDOUT):
             print('%d passwd %s test faild'%(self.COUNT,passwd))
         else:
             print('the correct passwd is:%s'%passwd)
-            queue.put(passwd)
-COUNT=0
-PASSWD=''
-q=Manager().Queue()
-def cracktest(passwd,q):
-    if subprocess.call(['unrar','t','-inul','-p%s'%passwd,'/tmp/ASTM_2015_Standards.rar','ASTM_2015_Standards/ASTM 2015 PART IA/ASTM 2015 Volume 01.01 Steel - Piping, Tubing, Fittings/A1014A1014M-10_Standard_Specification_for_Precipitation-Hardening_Bolting_(UNS_N07718)_for_High_Temperature_Service.pdf'],stdout = open('/dev/null','w'),stderr = subprocess.STDOUT):
-        print('%d passwd %s test faild'%(COUNT,passwd))
-    else:
-        print('the correct passwd is:%s'%passwd)
-        q.put(passwd)
-        return True
-with open('28GBwordlist.dic','r') as fp:
-    while True:
-        chunk_list=fp.readlines(102400000)#100MB
-        count=0
-        if chunk_list:
-            print('a new chunks start now!')
+            q.put(passwd)
+    def cracktest(self):
+        with open(self.dictfile,'r') as fp:
+            q=Manager().Queue()
+            if self.COUNT:
+                print('start continue with COUNT value lines')
+                for i in range(self.COUNT-os.cpu_count()):
+                    fp.readline()
+                self.COUNT=self.COUNT-os.cpu_count()
             while True:
-                if not q.empty():
-                    PASSWD=q.get()
-                    print('PASSWD=%s'%PASSWD)
-                    exit()
-                if count == len(chunk_list):
+                chunk_list=fp.readlines(10240000)#10MB
+                count=0
+                if chunk_list:
+                    print('a new chunks start now!')
+                    while True:
+                        if not q.empty():
+                            self.PASSWD=q.get()
+                            print('PASSWD=%s'%PASSWD)
+                            exit()
+                        elif count == len(chunk_list):
+                            break
+                        else:
+                            P=Pool(os.cpu_count())
+                            for i in range(os.cpu_count()):
+                                if count == len(chunk_list):
+                                    print('chunk end')
+                                    break
+                                else:
+                                    testpasswd=chunk_list[count]
+                                    count+=1
+                                    self.COUNT+=1
+                                    P.apply_async(self._subproc,args=(testpasswd[:-1],self.filename,q))
+                            P.close()
+                            P.join()
+                else:
+                    print('all passwd have been checked,exit now!')
                     break
-                P=Pool(os.cpu_count())
-                for i in range(os.cpu_count()):
-                    if count == len(chunk_list):
-                        print('chunk end')
-                        break
-                    testpasswd=chunk_list[count]
-                    count+=1
-                    COUNT+=1
-                    P.apply_async(cracktest,args=(testpasswd[:-1],q))
-                P.close()
-                P.join()
-        else:
-            print('all passwd have been checked,exit now!')
-            break
+if __name__=='__main__':
+    if len(sys.argv) > 1:
+        sss=cracker(sys.argv[1])
+        sss.cracktest()
+    else:
+        pass
